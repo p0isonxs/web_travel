42c5ed45e8b3f6a5716172280633e9cc91dc36ed
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2026-04-06 23:55:55 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { HelmetProvider } from 'react-helmet-async'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import PrivateRoute from './components/PrivateRoute'
 
 // Public Pages
 import Home from './pages/Home'
-import Packages from './pages/Packages'
+import OpenTrip from './pages/OpenTrip'
+import PrivateTrip from './pages/PrivateTrip'
 import PackageDetail from './pages/PackageDetail'
 import Booking from './pages/Booking'
 import Payment from './pages/Payment'
 import PaymentSuccess from './pages/PaymentSuccess'
+import Gallery from './pages/Gallery'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Blog from './pages/Blog'
@@ -28,40 +33,45 @@ import AdminTestimonials from './pages/admin/Testimonials'
 import AdminSettings from './pages/admin/Settings'
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/paket-wisata" element={<Packages />} />
-          <Route path="/paket/:id" element={<PackageDetail />} />
-          <Route path="/booking/:id" element={<Booking />} />
-          <Route path="/payment/:bookingId" element={<Payment />} />
-          <Route path="/payment/success" element={<PaymentSuccess />} />
-          <Route path="/tentang-kami" element={<About />} />
-          <Route path="/kontak" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogDetail />} />
-          <Route path="/login" element={<Login />} />
-          
-          {/* Admin Routes */}
-          <Route path="/admin" element={<PrivateRoute><AdminLayout /></PrivateRoute>}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="paket" element={<AdminPackages />} />
-            <Route path="booking" element={<AdminBookings />} />
-            <Route path="pembayaran" element={<AdminPayments />} />
-            <Route path="blog" element={<AdminBlog />} />
-            <Route path="galeri" element={<AdminGallery />} />
-            <Route path="testimoni" element={<AdminTestimonials />} />
-            <Route path="pengaturan" element={<AdminSettings />} />
-          </Route>
-          
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  )
+    return (
+          <HelmetProvider>
+                <AuthProvider>
+                        <Router>
+                                  <Routes>
+                                    {/* Public Routes */}
+                                              <Route path="/" element={<Home />} />
+                                              <Route path="/open-trip" element={<OpenTrip />} />
+                                              <Route path="/private-trip" element={<PrivateTrip />} />
+                                              <Route path="/paket/:id" element={<PackageDetail />} />
+                                              <Route path="/booking/:id" element={<Booking />} />
+                                              <Route path="/payment/:bookingId" element={<Payment />} />
+                                              <Route path="/payment/success" element={<PaymentSuccess />} />
+                                              <Route path="/galeri" element={<Gallery />} />
+                                              <Route path="/tentang-kami" element={<About />} />
+                                              <Route path="/kontak" element={<Contact />} />
+                                              <Route path="/blog" element={<Blog />} />
+                                              <Route path="/blog/:slug" element={<BlogDetail />} />
+                                              <Route path="/login" element={<Login />} />
+                                  
+                                    {/* Admin Routes */}
+                                              <Route path="/admin" element={<PrivateRoute><AdminLayout /></PrivateRoute>PrivateRoute>}>
+                                                            <Route index element={<AdminDashboard />} />
+                                                            <Route path="paket" element={<AdminPackages />} />
+                                                            <Route path="booking" element={<AdminBookings />} />
+                                                            <Route path="pembayaran" element={<AdminPayments />} />
+                                                            <Route path="blog" element={<AdminBlog />} />
+                                                            <Route path="galeri" element={<AdminGallery />} />
+                                                            <Route path="testimoni" element={<AdminTestimonials />} />
+                                                            <Route path="pengaturan" element={<AdminSettings />} />
+                                              </Route>Route>
+                                  
+                                              <Route path="*" element={<NotFound />} />
+                                  </Routes>Routes>
+                                  <ToastContainer position="top-right" autoClose={3000} />
+                        </Router>Router>
+                </AuthProvider>AuthProvider>
+          </HelmetProvider>HelmetProvider>
+        )
 }
 
-export default App
+export default App</HelmetProvider>
