b68a0efcad94762db70f4559d042f76af8b80879
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2026-04-08 04:09:12 +0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,24 +1,20 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
-import { getStorage } from 'firebase/storage'
-import { getAnalytics } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMlsiSMtsGDcn9uHeCYK5w_m6h8PmQEUA",
-  authDomain: "unit-9e4cc.firebaseapp.com",
-  projectId: "unit-9e4cc",
-  storageBucket: "unit-9e4cc.firebasestorage.app",
-  messagingSenderId: "465019289576",
-  appId: "1:465019289576:web:7f83d9fa2085ff7c0f6e5d",
-  measurementId: "G-J0HR5L2TS5"
-}
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
-export const auth = getAuth(app)
-export const storage = getStorage(app)
-export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null
-
-export default app
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export default app;
